7f70d77bba2d05f687bd0d0c5e3db77a5a88e341
barackmaund1 <pmaundu49@gmail.co>
2020-04-24 20:35:42 +0300

    creating and storing Api keys

---
+++ app/views.py
@@ -0,0 +1,19 @@
+from flask import render_template
+from app import app
+
+# Views
+@app.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+    title='Top Headlines'
+    return render_template('index.html')
+@app.route('/content/<article_id>')
+def content(''):
+
+    '''
+    View content page function that returns the news content page and its data
+    '''
+    return render_template('movie.html',id = article_id)    
